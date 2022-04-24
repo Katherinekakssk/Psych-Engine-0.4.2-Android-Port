06fb12fd60facb7cccbdbe9bed8a4e0b899f2717
Katherinekakssk <102576824+Katherinekakssk@users.noreply.github.com>
2022-04-24 10:40:38 -0500

    Add files via upload

---
+++ assets/preload/data/devils-gambit/script.lua
@@ -0,0 +1,16 @@
+
+
+
+
+
+local allowCountdown = false
+
+function opponentNoteHit()
+       cameraShake('game', 0.01, 0.3)
+
+       health = getProperty('health')
+    if getProperty('health') > 0.075 then
+       setProperty('health', health- 0.022);
+	end
+end
+
